a17e8f9451270641ae2156ff44b48a9491b9d89e
Angela Masajo <aabmasajo@gmail.com>
2020-11-19 20:59:04 -0600

    added quiz code

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ const store = {
     {
       question: 'When was the first color photo taken?',
       answers: [
-        '1861',
+        '1962',
         '1924',
         '1847',
-        '1962'
+        '1861'
       ],
       correctAnswer: '1861'
     },
@@ -38,9 +38,9 @@ const store = {
       question: 'What year was the first digital camera invented?',
       answers: [
         '1899',
-        '1975',
+        '1958',
         '1990',
-        '1958'
+        '1975'
       ],
       correctAnswer: '1975'
     },
@@ -57,128 +57,266 @@ const store = {
   ],
   quizStarted: false,
   questionNumber: 0,
-  score: 0,
   submittingAnswer: false,
-  
+  score: 0,
 
   currentQuestionState: {
-  answerArray: [];
+    answerArray: []
   }
 };
 
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material and access support for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
+
+
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
-function generateWelcomeScreen() {
+
+function generateWelcomeString() {
   return `
+  <header>
+  <h1>Photography Quiz</h1>
+  </header>
   <div class="opening">
-  <p>Welcome. Start the quiz below.</p>
     <form>
-        <button type="submit" id="startQuiz">START</button>
+      <p>Welcome. Start the quiz below.</p>
+      <button type="submit" id="beginQuiz" autofocus>START</button>
     </form>
   </div>
-  `;
-} 
+    `;
 
-function generateQuizQuestionsScreen(questionObject) {
+
+
+
+}
+function generateQuizInterfaceString(questionObject) {
   return `
-  <div class="quiz-questions">
-    <p>Question $(questionObject.index) of ${store.questions.length}</p>
-    
-    <p>
-      ${questionObject.question.question}
-    </p>
-    
-    <form>
+  <header>
+  <h1>Photography Quiz</h1>
+  </header>
+    <div class='quiz-questions'>
+      <p>Question ${questionObject.index} out of ${store.questions.length}</p>
+      <p>
+       ${questionObject.question.question}
+      </p>
+      <form>
       <ol type="a">
         ${generateQuizAnswers(questionObject.question.answers)}
       </ol>
-
-    <button type="submit" class="submit-answer">SUBMIT</button>
-    </form> 
-    <p>${store.score}</p>
-  </div>
-  `;
+      <button type="submit" class="submit-answer">Submit Answer</button>
+      </form> 
+      <p>Score: ${store.score}</p>
+    </div>
+    `;
 }
 
-function generateAnswerOutcome() {
+
+function generateAnswerResults() {
   let answerArray = store.currentQuestionState.answerArray;
 
   const buttons = {
-    next: '<button type="submit" class="next-question" autofocus>Next</button>',
-    result: '<button type ="submit" class="see-results" autofocus>See Results</button>'
+    next: `<button type="submit" class="next-question" autofocus>NEXT</button>`,
+    results: `<button type="submit" class="see-results" autofocus>SEE MY RESULTS</button>`
   };
 
-  let correctResponse = `${answerArray[1] is correct}`;
-  let incorrectResponse = `${answerArray[2]} is incorrect. The correct answer is ${answerArray[1]}`;
+  let correctResponse = `"${answerArray[1]}" is correct`;
+  let incorrectResponse = `"${answerArray[2]}" is not correct. The correct answer is "${answerArray[1]}"`;
 
-let lastQuestion = (store.questionNumber +1) === (store.questions.length);
+  let isLastQuestion = (store.questionNumber + 1) === (store.questions.length);
 
-
-//ask about this because i don't want to add this code if i don't understand
-return `
-    <div class="answer-response">
+  return `
+  <header>
+  <h1>Photography Quiz</h1>
+  </header>  
+  <div class="answer-response">
     <form>
     <p>${answerArray[0] === true ? correctResponse : incorrectResponse}</p>
     <p> Score: ${store.score}</p>
-  ${lastQuestion ? buttons.result : buttons.next}
+   ${isLastQuestion ? buttons.results : buttons.next}
     </form>
     </div>
-`;
+  `;
 }
 
+
+function generateQuizAnswers(answers) {
+  let answerArray = [];
+  answers.forEach(answer => {
+    answerArray.push(answer);
+  });
+  return answerArray.map(answer => stringifyAnswerArray(answer)).join('');
+}
+
+function stringifyAnswerArray(answer) {
+  let questionNumber = store.questionNumber;
+  let name = store.questions[questionNumber].answers.indexOf(answer);
+  return `
+  <li>
+      <div class="answer-container">
+      <input type="radio" name="answer" id="answer-${name}" data-answer="${answer}">
+      <label for="answer-${name}"> ${answer}</label>
+     
+      </div>
+    </li>
+  `;
+}
+
+function generateQuizResultsString() {
+  return `
+  <header>
+  <h1>Photography Quiz</h1>
+  </header>  
+  <div class='quiz-results'>
+      <p>
+        You finished the quiz. Thank you!
+         </p>
+          <p>You scored ${store.score} out of ${store.questions.length}. Press the button if you would like to redeem yourself.</p>            
+        <button class="restart-quiz">Restart Quiz</button>      
+    </div>   
+  `;
+}
 
 /********** RENDER FUNCTION(S) **********/
 
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
+function renderQuiz() {
+
+  if (store.quizStarted === false) {
+    if (store.questionNumber === store.questions.length) {
+      const quizResultsString = generateQuizResultsString();
+      $('main').html(quizResultsString);
+    } else {
+      const quizWelcomeInterfaceString = generateWelcomeString();
+      $('main').html(quizWelcomeInterfaceString);
+    }
+  } else {
+    if (store.submittingAnswer === false) {
+      const quizInterfaceString = generateQuizInterfaceString(currentQuestion());
+      $('main').html(quizInterfaceString);
+    } else if (store.submittingAnswer === true) {
+      const quizAnswerResponseString = generateAnswerResults();
+      $('main').html(quizAnswerResponseString);
+    }
+  }
+}
 
 
-
-//this is the current question
-function quizStart() {
+// Changes to quizStarted = true
+function startQuiz() {
   store.quizStarted = true;
 }
 
+// currentQuestion
 function currentQuestion() {
-  let index = store.questionNumber;
-  let questionObject = store.questions[i];
+  let questionNumber = store.questionNumber;
+  let questionObject = store.questions[questionNumber];
   return {
-    index: i + 1,
+    index: questionNumber + 1,
     question: questionObject
   };
 }
 
+// Go to the next question of the quiz
+// Model function changes state
+function nextQuestion() {
+  if (store.questionNumber < store.questions.length) {
+    store.questionNumber++;
+    store.submittingAnswer = false;
+  } else if (store.questionNumber === store.questions.length) {
+    store.quizStarted = false;
+  }
+}
+
+
+function validateCorrectAnswer() {
+  let radios = $('input:radio[name="answer"]');
+  let selectedAnswer = $('input[name="answer"]:checked').data('answer').toString();
+  let questionNumber = store.questionNumber;
+  let correctAnswer = store.questions[questionNumber].correctAnswer;
+
+  if (radios.filter(':checked').length === 0) {
+    alert('Please select your answer.');
+    return;
+  } else {
+    store.submittingAnswer = true;
+    if (selectedAnswer === correctAnswer) {
+      store.score += 1;
+      store.currentQuestionState.answerArray = [true, correctAnswer, selectedAnswer];
+    } else {
+      store.currentQuestionState.answerArray = [false, correctAnswer, selectedAnswer];
+    }
+  }
+}
+
+function seeResults() {
+  store.quizStarted = false;
+  store.questionNumber++;
+}
+
+function restartQuiz() {
+  store.score = 0;
+  store.quizStarted = false;
+  store.questionNumber = 0;
+  store.submittingAnswer = false;
+  store.currentQuestionState.answerArray = [];
+}
+
 /********** EVENT HANDLER FUNCTIONS **********/
-
 // These functions handle events (submit, click, etc)
+// Controller layer
 
+function handleBeginQuizSubmit() {
 
+  $('main').on('click', '#beginQuiz', (event) => {
+    event.preventDefault();
+    startQuiz();
+    renderQuiz();
+  });
+}
 
-/*
+function handleSubmitAnswer() {
+  $('main').on('click', '.submit-answer', (event) => {
+    event.preventDefault();
+    validateCorrectAnswer();
+    renderQuiz();
+  });
+}
 
-PSEUDOCODE
+function handleNextQuestionSubmit() {
+  $('main').on('click', '.next-question', (event) => {
+    event.preventDefault();
+    nextQuestion();
+    renderQuiz();
+  });
+}
 
-Things that need to happen 
-- Make the options clickable
-- separate the welcome page to the question page
-- when i click the START button, it needs to lead to the first question.
-- when i pick an answer,
+function handleSeeResultsSubmit() {
+  $('main').on('click', '.see-results', (event) => {
+    event.preventDefault();
+    seeResults();
+    renderQuiz();
+  });
+}
 
+function handleRestartQuizSubmit() {
+  $('main').on('click', '.restart-quiz', (event) => {
+    event.preventDefault();
+    restartQuiz();
+    renderQuiz();
+  });
+}
 
+function eventHandlers() {
+  handleBeginQuizSubmit();
+  handleSubmitAnswer();
+  handleNextQuestionSubmit();
+  handleRestartQuizSubmit();
+  handleSeeResultsSubmit();
+}
 
-*/
+// This function will launch all other functions after the page is loaded
+function handleQuiz() {
+  renderQuiz();
+  eventHandlers();
+}
+
+$(handleQuiz);
